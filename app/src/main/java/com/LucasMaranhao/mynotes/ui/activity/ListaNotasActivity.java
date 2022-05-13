c2a1a08f026e6779d99d10e0de7765700e04b676
Lucas Maranhão <Lucasvsm@gmail.com>
2022-05-13 12:33:26 -0300

    Criando a Recycler View e configurando seus onCreateViewHolder e o onBindViewHolder

--- app/src/main/java/com/LucasMaranhao/mynotes/ui/activity/ListaNotasActivity.java
+++ app/src/main/java/com/LucasMaranhao/mynotes/ui/activity/ListaNotasActivity.java
@@ -1,15 +1,16 @@
 package com.LucasMaranhao.mynotes.ui.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.LucasMaranhao.mynotes.R;
 import com.LucasMaranhao.mynotes.dao.NotaDAO;
 import com.LucasMaranhao.mynotes.model.Nota;
 import com.LucasMaranhao.mynotes.ui.adapter.ListaNotasAdapter;
+import com.LucasMaranhao.mynotes.ui.recyclerview.adapter.ListaNotaAdapter;
 
 import java.util.List;
 
@@ -20,16 +21,16 @@ public class ListaNotasActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_notas);
 
-        ListView listaNotas = findViewById(R.id.listView);
+        RecyclerView listaNotas = findViewById(R.id.lista_notas_recyclerview);
 
         NotaDAO dao = new NotaDAO();
         for (int i = 1; i <= 10000; i++){
-            dao.insere(new Nota("Título" + i, "Descrição" + i));
+            dao.insere(new Nota("Título " + i, "Descrição " + i));
         }
         List<Nota> todasNotas = dao.todos();
 
-        listaNotas.setAdapter(new ListaNotasAdapter(this, todasNotas));
-
-        
+        listaNotas.setAdapter(new ListaNotaAdapter(this, todasNotas));
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        listaNotas.setLayoutManager(layoutManager);
     }
 }
